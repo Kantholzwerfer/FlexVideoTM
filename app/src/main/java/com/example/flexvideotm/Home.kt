92bd4f0b51c4b89cc973c14a9054fc5d540d44d1
brian <brianwuesti1@gmail.com>
2023-06-19 19:34:24 +0200

    Brians Home

--- app/src/main/java/com/example/flexvideotm/Home.kt
+++ app/src/main/java/com/example/flexvideotm/Home.kt
@@ -1,6 +1,4 @@
 package com.example.flexvideotm
-//package com.gtappdevelopers.kotlingfgproject
-
 import android.annotation.SuppressLint
 import android.os.Bundle
 import android.os.Parcel
@@ -15,6 +13,9 @@ import com.jjoe64.graphview.GraphView
 import com.jjoe64.graphview.series.DataPoint
 import com.jjoe64.graphview.series.LineGraphSeries
 
+
+
+
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
@@ -25,6 +26,9 @@ private const val ARG_PARAM2 = "param2"
  * Use the [Home.newInstance] factory method to
  * create an instance of this fragment.
  */
+
+
+
 @SuppressLint("ParcelCreator")
 class Home() : Fragment(), Parcelable {
     // TODO: Rename and change types of parameters
@@ -36,20 +40,94 @@ class Home() : Fragment(), Parcelable {
         param2 = parcel.readString()
     }
 
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        arguments?.let {
-//            param1 = it.getString(ARG_PARAM1)
-//            param2 = it.getString(ARG_PARAM2)
-//        }
-//    }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    fun addDataPointToSeries(series: LineGraphSeries<DataPoint>, x: Double, y: Double) {
+        series.appendData(DataPoint(x, y), true, 100)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val seriesWeight = LineGraphSeries<DataPoint>(
+            arrayOf(
+                DataPoint(0.0, 75.0),
+                DataPoint(1.0, 75.3),
+                DataPoint(2.0, 74.8),
+                DataPoint(3.0, 74.3),
+                DataPoint(4.0, 74.9),
+                DataPoint(5.0, 74.0),
+                DataPoint(6.0, 74.2),
+                DataPoint(7.0, 73.9),
+                DataPoint(10.0, 73.5)
+            )
+        )
+
+        setupGraph(seriesWeight)
+
+
+        val lineGraphView: GraphView = requireView().findViewById(R.id.idGraphView)
+
+
+        //val seriesWeight = LineGraphSeries<DataPoint>(arrayOf(DataPoint(0.0, 75.0), DataPoint(1.0, 75.3)))
+        val x = 15.0
+        val y = 71.8
+
+        addDataPointToSeries(seriesWeight, x, y)
+    }
+
+
+    fun setupGraph(seriesWeight: LineGraphSeries<DataPoint>) {
+        val lineGraphView: GraphView = requireView().findViewById(R.id.idGraphView)
+
+
+        val seriesGoal = LineGraphSeries<DataPoint>(
+            arrayOf(
+                DataPoint(0.0, 73.0),
+                DataPoint(1.0, 73.0),
+                DataPoint(2.0, 73.0),
+                DataPoint(3.0, 73.0),
+                DataPoint(4.0, 73.0),
+                DataPoint(5.0, 73.0),
+                DataPoint(6.0, 73.0),
+                DataPoint(7.0, 73.0),
+                DataPoint(11.0, 73.0)
+            )
+        )
+
+        seriesWeight.color = resources.getColor(R.color.white)
+        seriesGoal.color = resources.getColor(R.color.black)
+
+        lineGraphView.addSeries(seriesWeight)
+        lineGraphView.addSeries(seriesGoal)
+
+        lineGraphView.viewport.isScrollable = true
+        lineGraphView.viewport.isScalable = true
+        lineGraphView.viewport.setScalableY(true)
+        lineGraphView.viewport.setScrollableY(true)
+        lineGraphView.viewport.isXAxisBoundsManual = true
+
+
+        lineGraphView.viewport.setMinX(0.0)
+        lineGraphView.viewport.setMaxX(8.0)
+        lineGraphView.viewport.setMinY(70.0)
+        lineGraphView.viewport.setMaxY(80.0)
+        lineGraphView.viewport.setYAxisBoundsManual(true)
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(param1)
+        parcel.writeString(param2)
+    }
+
+    override fun describeContents(): Int {
+        return 0
     }
 
     companion object {
@@ -71,121 +149,8 @@ class Home() : Fragment(), Parcelable {
                 }
             }
     }
+}
 
-// idee von chatgpt, die klasse komplett entfernen und durch einen setgraph() methode zu ersetzten
-    class HomeGraphics() : AppCompatActivity(), Parcelable {
 
-        //on below line we are creating
-        // variables for our graph view
-        lateinit var lineGraphView: GraphView
 
-        constructor(parcel: Parcel) : this() {
-
-        }
-
-        override fun onCreate(savedInstanceState: Bundle?) {
-            super.onCreate(savedInstanceState)
-            setContentView(R.layout.fragment_home)
-
-            // on below line we are initializing
-            // our variable with their ids.
-            lineGraphView = findViewById(R.id.idGraphView)
-
-            // on below line we are adding data to our graph view.
-            val seriesWeight: LineGraphSeries<DataPoint> = LineGraphSeries(
-                arrayOf(
-                    // on below line we are adding
-                    // each point on our x and y axis.
-                    DataPoint(0.0, 75.0),
-                    DataPoint(0.2, 1.0),
-                    DataPoint(2.0, 74.8),
-                    DataPoint(3.0, 74.3),
-                    DataPoint(4.0, 74.9),
-                    DataPoint(5.0, 74.0),
-                    DataPoint(6.0, 74.2),
-                    DataPoint(7.0, 73.9),
-                    DataPoint(8.0, 73.5)
-                )
-            )
-
-            // on below line we are adding data to our graph view.
-            val seriesGoal: LineGraphSeries<DataPoint> = LineGraphSeries(
-                arrayOf(
-                    // on below line we are adding
-                    // each point on our x and y axis.
-                    DataPoint(0.0, 73.0),
-                    DataPoint(0.4, 1.5),
-                    DataPoint(2.0, 73.0),
-                    DataPoint(3.0, 73.0),
-                    DataPoint(4.0, 73.0),
-                    DataPoint(5.0, 73.0),
-                    DataPoint(6.0, 73.0),
-                    DataPoint(7.0, 73.0),
-                    DataPoint(8.0, 73.0)
-                )
-            )
-
-            // on below line adding animation
-//            lineGraphView.animate()
-
-            // on below line we are setting scrollable
-            // for point graph view
-            lineGraphView.viewport.isScrollable = true
-
-            // on below line we are setting scalable.
-            lineGraphView.viewport.isScalable = true
-
-            // on below line we are setting scalable y
-            lineGraphView.viewport.setScalableY(true)
-
-            // on below line we are setting scrollable y
-            lineGraphView.viewport.setScrollableY(true)
-
-            // on below line we are setting color for series.
-            seriesWeight.color = R.color.teal_200
-            seriesGoal.color = R.color.black
-
-            // on below line we are adding
-            // data series to our graph view.
-            lineGraphView.addSeries(seriesWeight)
-            lineGraphView.addSeries(seriesGoal)
-
-            // Automatische Anpassung der Skalierung
-            lineGraphView.viewport.isYAxisBoundsManual = true
-            lineGraphView.viewport.setMinX(0.0)
-            lineGraphView.viewport.setMaxX(8.0)
-            lineGraphView.viewport.setMinY(73.0)
-            lineGraphView.viewport.setMaxY(75.5)
-
-        }
-
-        override fun writeToParcel(parcel: Parcel, flags: Int) {
-
-        }
-
-        override fun describeContents(): Int {
-            return 0
-        }
-
-        companion object CREATOR : Parcelable.Creator<Home> {
-            override fun createFromParcel(parcel: Parcel): Home {
-                return Home(parcel)
-            }
-
-            override fun newArray(size: Int): Array<Home?> {
-                return arrayOfNulls(size)
-            }
-        }
-    }
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeString(param1)
-        parcel.writeString(param2)
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    }
 
