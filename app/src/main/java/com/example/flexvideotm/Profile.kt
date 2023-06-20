56fa34db2ae74e8214c00ff858c5d1800dc6375b
Kantholzwerfer <kantholzwerfer71020@web.de>
2023-06-20 22:53:01 +0200

    - Profil Anpassungen

--- app/src/main/java/com/example/flexvideotm/Profile.kt
+++ app/src/main/java/com/example/flexvideotm/Profile.kt
@@ -41,7 +41,6 @@ private lateinit var scrollMaster: ScrollView
 private lateinit var homeFragment: Home
 
 
-
 /**
  * A simple [Fragment] subclass.
  * Use the [Profile.newInstance] factory method to
